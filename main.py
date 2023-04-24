48e003e9981f4315dbcf6373a80ff37fe13d6e48
GedeGustav <gust4398@elev.tec.dk>
2023-04-24 11:11:01 +0200

    dnank

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ jinja = SanicJinja2(app)
 
 #TODO: jeres dict "globals" skal indeholde en key "number" og en tilsvarende værdi 0
 
-globals = {}
+globals = {"number": 0}
 
 #Dette endpoint giver en respons ved en http GET request der peger på "localhost:8080/"
 #Responsen svarer til indholdet af index.html og pga. at index.html er et jinja template, 
@@ -29,15 +29,26 @@ async def index(request):
 @app.post("/add")
 async def add_number(request):
      #TODO: Add to number here 
+     globals["number"] += 1
      return redirect(f"/")
 
 #TODO: Opret en POST funktion der kan trække 1 fra nummeret i jeres dict
+@app.post("/remove")
+async def subtrackt_number(request):
+     #TODO: subtract to number here 
+     globals["number"] -= 1
+     return redirect(f"/")
 
 #TODO: Funktionen til at trække fra skal kaldes i en form fra jeres "index.html"
 
 #TODO: Prøv at lave et forloop i en HTML fil vha. statement-delimiteren-{% %}
 #Forloopet kan f.eks. køre på en range fra 0 til "number"
 #I skal bruge {%endfor%} for at jinja kan afgøre hvornår forloops blokken er slut
+
+  
+
+
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080)
